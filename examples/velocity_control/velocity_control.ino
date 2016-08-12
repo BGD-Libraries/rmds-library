4307365bcf27f9d79190cfc628c876325c06cee6
Yda-Young <158964215@qq.com>
2016-08-12 11:28:16 +0800

    修改速度控制的示例程序

--- examples/velocity_control/velocity_control.ino
+++ examples/velocity_control/velocity_control.ino
@@ -2,8 +2,6 @@
 #include <mcp_can.h>
 #include <SPI.h>
 
-#define MAX_VELOCITY 5000
-
 /*CAN 配置*/
 const int SPI_CS_pin = 40;
 MCP_CAN CAN(SPI_CS_pin);
@@ -16,57 +14,48 @@ Crmds motor1(0x01);
 
 void setup()
 {
-	Serial.begin(115200);
-	/*CAN 初始化*/
-	while(1){
-		if(CAN_OK == CAN.begin(CAN_1000KBPS)){
-			Serial.println("CAN BUS Shield init ok!");
-			break;
-		}
-		else{
-			Serial.println("CAN BUS Shield init fail");
-			Serial.println("Init CAN BUS Shield again");
-			delay(100);
-		}
-	}
+    Serial.begin(115200);
+    /*CAN 初始化*/
+    while(1){
+        if(CAN_OK == CAN.begin(CAN_1000KBPS)){
+            Serial.println("CAN BUS Shield init ok!");
+            break;
+        }
+        else{
+            Serial.println("CAN BUS Shield init fail");
+            Serial.println("Init CAN BUS Shield again");
+            delay(100);
+        }
+    }
 
-	/*电机驱动板初始化*/
-	motor1.reset();
-	motor1.write_data(&CAN_ID, can_tx_data);
-	CAN.sendMsgBuf(CAN_ID, 0, 8, can_tx_data);
-	delay(500);  //复位后需要等待500ms
-	/*选择PWM模式*/
-	//motor1.set_mode(ENTER_CURRENT_VELOCITY_MODE);
-	motor1.set_mode(ENTER_PWM_VELOCITY_MODE);
-	motor1.write_data(&CAN_ID, can_tx_data);
-	CAN.sendMsgBuf(CAN_ID, 0, 8, can_tx_data);
-	delay(500);  //设置模式后需要等待500ms
+    /*电机驱动板初始化*/
+    motor1.reset();
+    motor1.write_data(&CAN_ID, can_tx_data);
+    CAN.sendMsgBuf(CAN_ID, 0, 8, can_tx_data);
+    delay(500);  //复位后需要等待500ms
+    /*配置反馈*/
+    motor1.config(1);
+    motor1.write_data(&CAN_ID, can_tx_data);
+    CAN.sendMsgBuf(CAN_ID, 0, 8, can_tx_data);
+    delay(500);
+    /*选择PWM模式*/
+    //motor1.set_mode(ENTER_CURRENT_VELOCITY_MODE);
+    motor1.set_mode(ENTER_PWM_VELOCITY_MODE);
+    motor1.write_data(&CAN_ID, can_tx_data);
+    CAN.sendMsgBuf(CAN_ID, 0, 8, can_tx_data);
+    delay(500);  //设置模式后需要等待500ms
+
+    motor1.set_velocity(5000);  //速度单位RPM
+    motor1.write_data(&CAN_ID, can_tx_data);
+    CAN.sendMsgBuf(CAN_ID, 0, 8, can_tx_data);
+    delay(2); //最短周期2ms
 }
 
 void loop()
 {
-	for (int x=0;x<MAX_VELOCITY;x++) {
-		motor1.set_velocity(x);
-		motor1.write_data(&CAN_ID, can_tx_data);
-		CAN.sendMsgBuf(CAN_ID, 0, 8, can_tx_data);
-		delay(2); //最短周期2ms
-	}
-	for (int x=MAX_VELOCITY;x>0;x--) {
-		motor1.set_velocity(x);
-		motor1.write_data(&CAN_ID, can_tx_data);
-		CAN.sendMsgBuf(CAN_ID, 0, 8, can_tx_data);
-		delay(2); //最短周期2ms
-	}
-	for (int x=0;x>-MAX_VELOCITY;x--) {
-		motor1.set_velocity(x);
-		motor1.write_data(&CAN_ID, can_tx_data);
-		CAN.sendMsgBuf(CAN_ID, 0, 8, can_tx_data);
-		delay(2); //最短周期2ms
-	}
-	for (int x=-MAX_VELOCITY;x<0;x++) {
-		motor1.set_velocity(x);
-		motor1.write_data(&CAN_ID, can_tx_data);
-		CAN.sendMsgBuf(CAN_ID, 0, 8, can_tx_data);
-		delay(2); //最短周期2ms
-	}
+    if(CAN_MSGAVAIL == CAN.checkReceive()) {            // check if data coming
+        CAN.readMsgBufID(&CAN_ID,&CAN_len,can_rx_data);   //接收can数据
+        motor1.read_data(&CAN_ID,can_rx_data);
+        Serial.println(motor1.get_real_velocity());
+    }
 }
